1dc06a1be22b3ccdd7a90932d9a66c89a8020c0f
Poovarasan <poovarasanm0909@gmail.com>
2023-12-17 22:33:19 +0530

    deployed

--- src/content/Gemini_api.jsx
+++ src/content/Gemini_api.jsx
@@ -1,47 +1,62 @@
-import React,{Component} from "react";
+import React from "react";
 import {GoogleGenerativeAI} from "@google/generative-ai";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
+import {faPaperPlane} from '@fortawesome/free-solid-svg-icons'
+import jsonData from '../../package.json'
 
-class gemini_api extends React.Component{
+
+class gemini_api extends React.Component {
     constructor(props) {
         super(props);
-        this.state ={
-            response : ''
+        this.state = {
+            response: '',
+            question: ''
         };
     }
-    // componentDidMount() {
-    //
-    // }
-
 
     getResponseFromAI = (prompt) => {
-        const { GoogleGenerativeAI } = require("@google/generative-ai");
-        const genAI = new GoogleGenerativeAI('AIzaSyD3hYO3Ese95XivATRhmJxLQBaQl-e5JTs');
+        const {GoogleGenerativeAI} = require("@google/generative-ai");
+        const genAI = new GoogleGenerativeAI(jsonData.authKey);
+
         async function run() {
-            const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+            const model = genAI.getGenerativeModel({model: "gemini-pro"});
             const result = await model.generateContent(prompt);
             const response = await result.response;
             const text = response.text();
             return response.text();
         }
-        run().then(r =>   this.setState ({response : r}));
+
+        run().then(r => this.setState({response: r}));
     }
 
     render() {
-          return(
-              <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh'}}>
-                  <div id="res" className="response_container">{this.state.response}</div>
-                  <br/>
-                  <div>
-                  <input type="text" id={"prompt_inputs"}/>
-                  <button className = "sent_button" onClick={() => {
-                      let prompt = document.getElementById("prompt_inputs").value
-                      this.getResponseFromAI(prompt)
-                  }} > <FontAwesomeIcon icon={faPaperPlane} color={'green'} fontSize={40} />
-                  </button></div>
-              </div>
-          );
+        return (
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: '100vh'
+            }}>
+                <div id="res" className="response_container">
+                    <div>{this.state.question}</div>
+                    <div>{this.state.response}</div>
+
+                </div>
+                <br/>
+                <div>
+                    <input type="text" id={"prompt_inputs"}/>
+                    <button className="sent_button" onClick={() => {
+                        let prompt = document.getElementById("prompt_inputs").value
+                        document.getElementById("prompt_inputs").value = ''
+                        this.getResponseFromAI(prompt)
+                        // eslint-disable-next-line react/no-direct-mutation-state
+                        this.state.question = prompt
+                    }}><FontAwesomeIcon icon={faPaperPlane} color={'green'} fontSize={40}/>
+                    </button>
+                </div>
+            </div>
+        );
     }
 }
 
