65b2324ea5d98ed4558865eb646bb962fcf48484
Poovarasan <poovarasanm0909@gmail.com>
2023-12-21 23:03:11 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import Gemini_api from './content/Gemini_api'
 import Background from "./background/Background";
 import './css/background.css'
 import './css/contentPage.css'
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
       <div className={"container"}>
+          <Helmet>
+              <meta charSet="utf-8" />
+              <title>Your React App</title>
+              <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+              {/* Other meta tags */}
+          </Helmet>
           <Background/>
           <div style={{position: 'absolute', top: 0, right: 0, width: '100%', height: '100%'}}>
               {/* eslint-disable-next-line react/jsx-pascal-case */}
