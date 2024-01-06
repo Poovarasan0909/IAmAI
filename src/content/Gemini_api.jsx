94b244bb946c4205abaa7c8b141d03775cad5db0
Poovarasan <poovarasanm0909@gmail.com>
2024-01-06 23:47:27 +0530

    ui changes

--- src/content/Gemini_api.jsx
+++ src/content/Gemini_api.jsx
@@ -15,53 +15,6 @@ class gemini_api extends React.Component {
         };
     }
 
-     // reformatResponse =(response) =>  {
-     //    // Extract and bold the title
-     //    const titleMatch = response.match(/^(.*):/);
-     //    const title = titleMatch ? `<b>${titleMatch[1]}</b>` : "";
-     //
-     //    // Split the response into lines, removing empty lines
-     //    const lines = response.split("\n").filter((line) => line !== "");
-     //
-     //    // Apply formatting rules
-     //    const formattedLines = lines.map((line) => {
-     //        if (line.startsWith("- ")) {
-     //            return `<li>> ${line.slice(2)}</li>`; // Convert hyphens to bullet points
-     //        } else if (line.startsWith("```")) {
-     //            return `<pre><code>${line.slice(3, -3)}</code></pre>`; // Convert code blocks
-     //        } else {
-     //            return line;
-     //        }
-     //    });
-
-        // Join the formatted lines, prepend the title, and return
-    //     return title + formattedLines.join("\n");
-    // }
-
-    // reformatResponse = (response)=>{
-    //     return response
-    //         .split('\n\n')
-    //         .map((line) => {
-    //             // Bold all level 1 headings (lines starting with '**')
-    //             if (line.startsWith('**')) {
-    //                 return `<h1>${line.slice(2,-2)}</h1>`;
-    //             }
-    //
-    //             // Italicize all level 2 headings (lines starting with '*')
-    //             if (line.startsWith('*')) {
-    //                 return `<h2>${line.slice(1,-1)}</h2>`;
-    //             }
-    //
-    //             // Convert hyphens to bullet points
-    //             if (line.startsWith('- ')) {
-    //                 return `<li>${line.slice(2)}</li>`;
-    //             }
-    //
-    //             // Keep other lines as paragraphs
-    //             return <p>{line}</p>;
-    //         })
-    // }
-
     getResponseFromAI = (prompt) => {
         this.setState({loading: true});
         const {GoogleGenerativeAI} = require("@google/generative-ai");
@@ -77,10 +30,7 @@ class gemini_api extends React.Component {
 
         run().then(res => {
             const response = this.reformatResponse(res);
-            // document.getElementById("response").innerHTML = response;
-            // const paragraphs = response.split('\n\n');
             console.log("formatted response -->",response)
-            document.getElementById("response_element").innerHTML = ''
             response.map((element) =>
                 document.getElementById("response_element").appendChild(element)
             )
@@ -93,15 +43,16 @@ class gemini_api extends React.Component {
         const lines = text.split('\n');
         console.log(lines, "linessss")
         const formattedResponse = [];
-        let spaceStarCheck = /^\s*\*/gm;
-        let spaceHypenCheck = /^\s*-/gm;
 
         let isCodeFirstLine = false;
         let isCodeLine = '';
         let codeContainer = document.createElement('div');
-        codeContainer.className = "code_block";
+        codeContainer.className = "code-container";
 
         lines.forEach((line, index) => {
+            let spaceStarCheck = /^\s*\*/gm;
+            let spaceHypenCheck = /^\s*-/gm;
+            let startWithDigitStar = /^\s*\d+\.\s+\*\*/gm;
             const codeElement = document.createElement('code');
             const preElement = document.createElement('pre');
             line = line.replace(/^s+/, '')
@@ -113,7 +64,7 @@ class gemini_api extends React.Component {
                     codeContainer.appendChild(preElement)
                     formattedResponse.push(codeContainer)
                     codeContainer = document.createElement('div');
-                    codeContainer.className = "code_block";
+                    codeContainer.className = "code-container";
                     isCodeLine = 'oneLine'
                 } else {
                     if (isCodeLine === '') {
@@ -128,7 +79,7 @@ class gemini_api extends React.Component {
                         codeContainer.appendChild(preElement)
                         formattedResponse.push(codeContainer)
                         codeContainer = document.createElement('div');
-                        codeContainer.className = "code_block";
+                        codeContainer.className = "code-container";
                         isCodeLine = 'end';
                     }
                 }
@@ -140,30 +91,29 @@ class gemini_api extends React.Component {
                 codeContainer.appendChild(preElement)
             }
             if ([''].includes(isCodeLine)) {
-                if (line.startsWith("**")) {
+                if (line.startsWith("**") && line.endsWith("**")) {
                     const customElement = document.createElement('div');
-                    customElement.innerHTML = '<strong style={{color: \'red\'}}>' + line.replace(/\*\*/g, '') + '</strong>';
+                    customElement.innerHTML = line.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong> &ensp;');
                     formattedResponse.push(customElement);
                 } else if (line.startsWith("- **") || line.startsWith("* **")) {
-                    let starLine = line.replace(/^[*-]/g, '').split('**');
+                    let starLine = line.replace(/^[*-]/g, '').replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>&ensp;');
                     const customElement = document.createElement('div');
-                    customElement.innerHTML = '<li key={index}><strong>' + starLine[1] + '</strong>' + '&nbsp;' +
-                        '<small>' + starLine[2] + '</small></li>';
+                    customElement.innerHTML = '<li>'+starLine+'</li>'
                     // eslint-disable-next-line no-useless-concat
                     formattedResponse.push(customElement);
-                } else if (line.startsWith("- ") || line.startsWith("* ")) {
+                    formattedResponse.push(document.createElement('br'))
+                }else if (line.startsWith("- ") || line.startsWith("* ")) {
                     const customElement = document.createElement('div');
+                    customElement.style = "margin:6px;"
                     customElement.innerHTML = '<li key={index}>' + line.substring(2) + '</li>';
                     formattedResponse.push(customElement);
                 } else if (spaceStarCheck.test(line) || spaceHypenCheck.test(line)) {
                     const customElement = document.createElement('div');
-                    customElement.innerHTML = '<li key={index}>' + line.replace(/^[-*]/, '') + '</li>';
+                    customElement.innerHTML = '<li key={index}>' + line.replace(/^\s+/,'').replace(/^[-*]/, '').replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>&ensp;') + '</li>';
                     formattedResponse.push(customElement)
-                } else if (line.length === 0) {
-                    formattedResponse.push(document.createElement('br'))
                 } else {
                     const customElement = document.createElement('div');
-                    customElement.innerHTML = '<p key={index}>' + line + '</p>';
+                    customElement.innerHTML = '<p key={index}>' + line.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong> &ensp;') + '</p>';
                     formattedResponse.push(customElement);
                 }
             }
@@ -175,6 +125,7 @@ class gemini_api extends React.Component {
 
     handleKeyDown = (event) => {
         if(event.key === 'Enter')  {
+            document.getElementById("response_element").innerHTML = ''
             let prompt = document.getElementById("prompt_inputs").value
             if(prompt.length > 0) {
             document.getElementById("prompt_inputs").value = ''
@@ -185,15 +136,6 @@ class gemini_api extends React.Component {
     }
 
     render() {
-        const titleRegex = /\*\*(.*?)\*\*/g;
-        const codeRegex = /```(.*?)```/g;
-        //Todo: want to do
-        const codeFirstRegex = /```/g;
-
-        // this.state.response.map((para) => (
-        //     console.log(para, codeRegex.test(para).toString(), titleRegex.test(para).toString(), !codeRegex.test(para).toString())
-        // ))
-
         return (
             <div style={{
                 display: 'flex',
